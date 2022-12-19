2f0e1bc822baf501cc8a64494265d2d7b077509a
Junyuan Lau <junyuan@Junyuans-New-MBP.local>
2022-12-19 13:17:00 -0800

    First commit

---
+++ jaffle_shop/models/example/my_first_dbt_model.sql
@@ -0,0 +1,19 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+select *, 1 as "customer_id"
+from "postgres".public._airbyte_raw_test
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
